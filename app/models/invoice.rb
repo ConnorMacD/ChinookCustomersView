b72e43b87f1a874d9f14a5b993014944bfab2cd9
Connor MacDonald <cmacd.94@gmail.com>
2015-03-12 11:00:13 -0300

    add pie chart for invoice functionality

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,13 @@ class Invoice < ActiveRecord::Base
 
     belongs_to :customer, :class_name => 'Customer', :foreign_key => :CustomerId
     has_many :invoicelines, :class_name => 'InvoiceLine', :foreign_key => :InvoiceId
+
+    def rockdata
+      self.invoicelines.track.each do |t|
+        if t.GenreId == 1
+          @rock += 1
+        end
+      end
+      @rock
+    end
 end
